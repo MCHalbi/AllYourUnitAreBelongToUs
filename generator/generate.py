431400aabe75c672931ff3271bb6438144c575dd
Lukas Halbritter <halbi93@gmx.de>
2020-12-13 01:05:33 +0100

    Added generator which generates the pythonunits package from a json file.

---
+++ generator/generate.py
@@ -0,0 +1,54 @@
+# Author: Lukas Halbritter <halbritl@informatik.uni-freiburg.de>
+# Copyright 2020
+import argparse
+import json
+import logging.config
+from generator import Generator
+
+with open('logging.json') as json_file:
+    logging_configuration = json.load(json_file)
+
+logging.config.dictConfig(logging_configuration)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Generates the pythonunit package from a json file.')
+
+    parser.add_argument(
+        '-s', '--source',
+        default='./quantities.json',
+        type=str,
+        help='The path to the json file from which the package should be '
+             'generated.')
+
+    parser.add_argument(
+        '-d', '--templates',
+        default='./templates/',
+        type=str,
+        help='The path to the template directory with templates for the module '
+             'files.')
+
+    parser.add_argument(
+        '-t', '--target',
+        default='../pythonunits/',
+        type=str,
+        help='The path to the directory in which the package should be '
+             'generated. If the directory does not exist, it is created.')
+
+    parser.add_argument(
+        '-f', '--force',
+        action='store_true',
+        help='Overwrite existing files and directories.')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
+    generator = Generator(args.source, args.target, args.templates)
+    generator.run(args.force)
+
+if __name__ == '__main__':
+    main()
