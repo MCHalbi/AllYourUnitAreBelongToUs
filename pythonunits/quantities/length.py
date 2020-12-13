d66515f6392872b313d607040ff4a3ab97443b8a
Lukas Halbritter <halbi93@gmx.de>
2020-12-13 21:32:03 +0100

    Added basic operator implementations.

--- pythonunits/quantities/length.py
+++ pythonunits/quantities/length.py
@@ -48,6 +48,54 @@ class Length:
     def __repr__(self):
         return str(self)
 
+    # Comparison operators
+    def __eq__(self, other):
+        return self._value == other.as_unit(self._unit)
+
+    # Unary operators
+    def __neg__(self):
+        return Length(-self._value, self._unit)
+
+    # Arithmetic operators
+    def __add__(self, other):
+        if type(other) is Length:
+            return Length(self._value + other.as_unit(self._unit), self._unit)
+
+        self._raise_type_error_for_undefined_operator(other, '+')
+
+    def __sub__(self, other):
+        if type(other) is Length:
+            return Length(self._value - other.as_unit(self._unit), self._unit)
+
+        self._raise_type_error_for_undefined_operator(other, '-')
+
+    def __mul__(self, other):
+        if type(other) in (float, int):
+            return Length(self._value * other, self._unit)
+
+        self._raise_type_error_for_undefined_operator(other, '*')
+
+    def __rmul__(self, other):
+        if type(other) in (float, int):
+            return self * other
+
+        self._raise_type_error_for_undefined_operator(other, '*')
+
+    def __truediv__(self, other):
+        if type(other) is Length:
+            return (self._get_value_in_base_unit()
+                    / other.as_unit(other.base_unit))
+        if type(other) in (float, int):
+            return Length(self._value / other, self._unit)
+
+        self._raise_type_error_for_undefined_operator(other, '/')
+
+    def _raise_type_error_for_undefined_operator(
+            self, other, operator: str) -> None:
+        raise TypeError(
+            'unsupported operand type(s) for {0}: \'{1}\' and \'{2}\''
+            .format(operator, type(self).__name__, type(other).__name__))
+
     @staticmethod
     def zero() -> 'Length':
         return Length(0, Length.base_unit)
@@ -63,6 +111,8 @@ class Length:
     def as_unit(self, unit: LengthUnit) -> float:
         if unit == self._unit:
             return self._value
+
+        return self._get_value_as(unit)
 
     def to_unit(self, unit: LengthUnit) -> 'Length':
         converted_value = self._get_value_as(unit)
